6d2f61348030bec950a270c15502f7e5c9cbb259
Davi Simão <davbsimao@gmail.com>
2026-05-20 11:50:12 -0300

    move database config to environment variables

--- database/connection.py
+++ database/connection.py
@@ -1,9 +1,21 @@
+from os import getenv
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-db = create_engine('mysql+pymysql://root:mysql123@localhost/sistema_pizzaria')
+
+load_dotenv()
+
+DB_USER = getenv('DB_USER')
+DB_PASSWORD = getenv('DB_PASSWORD')
+DB_HOST = getenv('DB_HOST')
+DB_NAME = getenv('DB_NAME')
+
+DATABASE_URL = f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
+
+db = create_engine(DATABASE_URL)
 
 Session = sessionmaker(bind=db)
 
 Base = declarative_base()
-
